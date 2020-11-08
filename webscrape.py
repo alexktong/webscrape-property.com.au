8a46224a5c4b9dc83199a232b46ab07408791a56
alexktong.92@gmail.com <alexktong.92@gmail.com>
2020-11-08 19:46:45 +1100

    Convert code to object-oriented

--- webscrape.py
+++ webscrape.py
@@ -3,35 +3,48 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime
+import pandas as pd
 
 
-def main(city, num_rooms, min_price=50, max_price=5000, min_car=0, min_bathroom=1, surrounding_suburbs=False):
+class PropertySearch:
 
-    # url component for selection of studio (i.e. no room) is null
-    num_rooms_url = ''
-    if num_rooms > 0:
-        num_rooms_url = f'with-{num_rooms}-bedrooms'
+    def __init__(self, city, num_rooms, min_price=50, max_price=5000, min_car=0, min_bathroom=1, surrounding_suburbs=False):
+        self.city = city
+        self.num_rooms = num_rooms
+        self.min_price = min_price
+        self.max_price = max_price
+        self.min_car = min_car
+        self.min_bathroom = min_bathroom
+        self.surrounding_suburbs = surrounding_suburbs
 
-    address_list = []
-    price_list = []
-    url_list = []
+    def search_url(self, page_num):
 
-    # scraps up to page 20
-    for page_num in range(1, 21):
+        # url component for selection of studio (i.e. no room) is null
+        num_rooms_url = ''
+        if self.num_rooms > 0:
+            num_rooms_url = f'with-{self.num_rooms}-bedrooms'
 
         # construct URL based on user parameters
         url = f'https://www.property.com.au/rent/' \
               f'{num_rooms_url}-between-' \
-              f'{min_price}-{max_price}-in-' \
-              f'{city},+' \
+              f'{self.min_price}-{self.max_price}-in-' \
+              f'{self.city},+' \
               f'/list-{page_num}?' \
-              f'numParkingSpaces={min_car}&' \
-              f'numBaths={min_bathroom}&' \
-              f'includeSurrounding={str(surrounding_suburbs).lower()}'
+              f'numParkingSpaces={self.min_car}&' \
+              f'numBaths={self.min_bathroom}&' \
+              f'includeSurrounding={str(self.surrounding_suburbs).lower()}'
 
-        response = requests.get(url)
+        return url
+
+    def results_page(self, page_num):
+
+        response = requests.get(self.search_url(page_num))
 
         html_soup = BeautifulSoup(response.text)
+
+        address_list = []
+        price_list = []
+        url_list = []
 
         if len(html_soup.find_all('div', {'id': 'noresults'})) == 0:
 
@@ -41,36 +54,58 @@ def main(city, num_rooms, min_price=50, max_price=5000, min_car=0, min_bathroom=
 
                 # store address of individual listing
                 address = result_element.find('div', {'class': 'vcard'}).find('a', {'rel': 'listingName'}).getText()
+                address_list.append(address)
 
                 # store price of individual listing
                 price = result_element.find('div', {'class': 'propertyStats'}).find('span', {'class': 'hidden'}).getText()
+                price_list.append(price)
 
                 # store URL of individual listing
                 url = result_element.find('div', {'class': 'vcard'}).find('a').get('href')
-
-                address_list.append(address)
-                price_list.append(price)
                 url_list.append(url)
 
-    # store listings in text file with naming convention 'rental-listings_YYYY-MM-DD.txt'
-    text_file = open(f'rental-listings_{datetime.datetime.now().date()}.txt', 'w')
+        df = pd.DataFrame({'Address': address_list, 'Price': price_list, 'URL': url_list})
 
-    if len(address_list) > 0:
+        return df
 
-        # rental listing stored in format 'ADDRESS - RENTAL PRICE - URL'
-        for ind, result in enumerate(address_list):
-            result_full = f'{result} - {price_list[ind]} - {url_list[ind]}'
-            text_file.write(result_full + '\n')
-    else:
-        text_file.write('No rental listings found that match search preferences. Please adjust preference selections.')
-    text_file.close()
+    def results(self):
+
+        # scraps up to page 20
+        df = []
+        for page_num in range(1, 21):
+            df_page_num = self.results_page(page_num)
+
+            df.append(df_page_num)
+
+        df = pd.concat(df).reset_index(drop=True)
+
+        return df
+
+    def results_save(self):
+        df = self.results()
+
+        if len(df) > 0:
+            df.to_csv(f'rental-listings_{datetime.datetime.now().date()}.csv', index=False)
+        else:
+            print('No rental listings found that match search preferences. Please adjust preference selections.')
 
 
-if __name__ == '__main__':
-    main(city='Chatswood', # if irrelevant listings found, try search 'CITY, STATE, POSTCODE' e.g. 'Chatswood, NSW, 2067'.
-         num_rooms=1,
-         min_price=350,
-         max_price=450,
-         min_car=0,
-         min_bathroom=1,
-         surrounding_suburbs=True)
+# if irrelevant listings found, try search 'CITY, STATE, POSTCODE' e.g. 'Chatswood, NSW, 2067'.
+a = PropertySearch(city='Chatswood',
+                   num_rooms=0,
+                   min_price=300,
+                   max_price=400,
+                   min_car=1,
+                   surrounding_suburbs=False)
+
+# return URL search string for search page
+a.search_url(3)
+
+# return results of one search page in Pandas dataframe
+a.results_page(3)
+
+# return all results in Pandas dataframe
+a.results()
+
+# store listings in CSV with default naming convention 'rental-listings_YYYY-MM-DD.txt'
+a.results_save()
